c94c98105b006f15ad070c9ca5d8e90cdfe4cd73
Jair-F <jair.fehlauer@gmail.com>
2026-02-14 11:54:01 +0200

    added things

--- far_socket.py
+++ far_socket.py
@@ -4,11 +4,17 @@ import struct
 
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind('0.0.0.0', 9999)
+    # s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+    s.settimeout(1)
+    s.bind(('0.0.0.0', 9999))
 
     while True:
         data = "sent from laptop - multicast"
         s.sendto(data.encode('utf-8'), ('239.1.1.2', 8888))
-        data, addr = s.recvfrom(1024)
-        print(f"Received from {addr}: {data.decode()}")
+        print(F"sent data to 239.1.1.2:8888")
+        try:
+            data, addr = s.recvfrom(1024)
+            print(f"Received from {addr}: {data.decode()}")
+        except TimeoutError:
+            pass
 
